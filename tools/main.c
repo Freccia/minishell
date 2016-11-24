beda87d471f1e78ca966bad017d468a6b70497c3
Freccia <Freccia@users.noreply.github.com>
2016-11-24 21:11:32 +0100

    UP::banner

--- tools/main.c
+++ tools/main.c
@@ -1,16 +1,16 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfabbro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: lfabbro <lfabbro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/21 17:15:54 by lfabbro           #+#    #+#             */
-/*   Updated: 2016/11/24 21:10:29 by lfabbro          ###   ########.fr       */
+/*   Created: 2016/11/24 20:55:19 by lfabbro           #+#    #+#             */
+/*   Updated: 2016/11/24 21:07:39 by lfabbro          ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
-#include "shell.h"
+#include "libft.h"
 
 static void		ft_banner(void)
 {
@@ -38,39 +38,10 @@ static void		ft_banner(void)
 	ft_putendl("      \\__\\/   ");
 }
 
-static void		ft_set_prompt(t_env *e, int ac, char **av)
-{
-	(void)ac;
-	(void)av;
-	e->prompt = ft_strdup("$>");
-}
 
-static void		ft_prompt(t_env e)
-{
-	ft_putstr(e.prompt);
-	ft_putchar(' ');
-}
 
-int				main(int ac, char **av, char **env)
+int		main(void)
 {
-	t_env	e;
-
-	e.env = env;
-	e.ft_errno = 0;
-	e.ft_exit = 0;
-	ft_set_prompt(&e, ac, av);
 	ft_banner();
-	while (e.exit == 0)
-	{
-		ft_prompt(e);
-		if (get_next_line(0, &e.line) > 0)
-		{
-			ft_parse_e.line(&e, e.line);
-			free(e.line);
-			e.line = NULL;
-		}
-	}
-	if (e.line)
-		free(e.line);
-	return (e.ft_errno);
+	return (0);
 }
