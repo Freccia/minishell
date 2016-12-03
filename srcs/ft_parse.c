836bb00aed1df20762c7f0591bc346fce8ea7bb7
Freccia <Freccia@users.noreply.github.com>
2016-12-03 02:02:04 +0000

    UP::

--- srcs/ft_parse.c
+++ srcs/ft_parse.c
@@ -12,37 +12,6 @@
 
 #include "shell.h"
 
-/*
- * $1, $2, $3, ... are the positional parameters.
- * "$@" is an array-like construct of all positional parameters, {$1, $2, $3 ...}.
- * "$*" is the IFS expansion of all positional parameters, $1 $2 $3 ....
- * $# is the number of positional parameters.
- * $- current options set for the shell.
- * $$ pid of the current shell (not subshell).
- * $_ most recent parameter (or the abs path of the command to start the current shell immediately after startup).
- * $IFS is the (input) field separator.
- * $? is the most recent foreground pipeline exit status.
- * $! is the PID of the most recent background command.
- * $0 is the name of the shell or shell script.
- */
-
-/*
-   static int		ft_subspecials(char ***cmd)
-   {
-   int		i;
-
-   i = -1;
-   while (*cmd[++i])
-   {
-   if (ft_strstr(*cmd[i], "$$"))
-   {
-   ft_subspec();
-   }
-   }
-   return (0);
-   }
-   */
-
 static char		**ft_parse_cmd(t_env *e)
 {
 	char	**cmds;
