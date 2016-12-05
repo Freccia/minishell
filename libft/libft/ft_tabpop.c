694a19aa4bddb20c3738d037d782a72934882f86
Freccia <Freccia@users.noreply.github.com>
2016-12-05 21:17:03 +0100

    ++libft

---
+++ libft/libft/ft_tabpop.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tabpop.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfabbro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/24 16:46:56 by lfabbro           #+#    #+#             */
+/*   Updated: 2016/11/25 17:46:00 by lfabbro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		**ft_tabpop(char **tab, size_t pop)
+{
+	char	**new;
+	size_t	i;
+
+	new = NULL;
+	i = -1;
+	if (tab)
+	{
+		new = ft_tabnew(ft_tablen(tab) - 1);
+		while (++i < pop && tab[i])
+		{
+			new[i] = ft_strdup(tab[i]);
+		}
+		while (tab[++i])
+		{
+			new[i - 1] = ft_strdup(tab[i]);
+		}
+		new[i - 1] = NULL;
+	}
+	return (new);
+}
