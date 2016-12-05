d08bdc86d0474925708ed7d79bd85c7caf4e3143
Freccia <Freccia@users.noreply.github.com>
2016-12-05 18:00:07 +0100

    up: echo fix

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -6,57 +6,11 @@
 /*   By: lfabbro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/01 13:38:01 by lfabbro           #+#    #+#             */
-/*   Updated: 2016/12/05 17:25:57 by lfabbro          ###   ########.fr       */
+/*   Updated: 2016/12/05 17:59:59 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
-
-static int	ft_print_escape(char c)
-{
-	if (c == '\\')
-		ft_putchar('\\');
-	else if (c == 'a')
-		ft_putchar('\a');
-	else if (c == 'b')
-		ft_putchar('\b');
-	else if (c == 'c')
-		return (1);
-	else if (c == 'f')
-		ft_putchar('\f');
-	else if (c == 'n')
-		ft_putchar('\n');
-	else if (c == 'r')
-		ft_putchar('\r');
-	else if (c == 't')
-		ft_putchar('\t');
-	else if (c == 'v')
-		ft_putchar('\v');
-	else
-		return (-1);
-	return (0);
-}
-
-static int	ft_subs_escape(char *str)
-{
-	int		i;
-	int		ret;
-
-	i = -1;
-	while (str[++i])
-	{
-		if (str[i] == '\\')
-		{
-			if ((ret = ft_print_escape(str[i + 1])) == 1)
-				return (1);
-			else if (ret == 0)
-				++i;
-		}
-		else
-			ft_putchar(str[i]);
-	}
-	return (0);
-}
 
 int			ft_echo(t_env *e)
 {
@@ -64,12 +18,13 @@ int			ft_echo(t_env *e)
 	int		nl;
 
 	i = 0;
-	nl = 0;
+	if ((nl = ft_strequ(e->cmd[1], "-n")))
+		++i;
 	while (e->cmd[++i])
 	{
-		if (i > 1 && i < (int)e->cmd_len)
+		if ((!nl && i == 1) || (i > 2 && i < (int)e->cmd_len))
 			ft_putchar(' ');
-		nl += ft_subs_escape(e->cmd[i]);
+		ft_putstr(e->cmd[i]);
 	}
 	if (!nl)
 		ft_putchar('\n');
